4774f823f264b8ad0c3ca727486c522bc9ba263d
Davis Ford <ford_davis@bah.com>
2017-12-09 10:25:14 -0500

    added google verification and switched to parkcenterautomotive.com

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -7,4 +7,4 @@
 #find ./images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
 
 # Upload to S3
-aws s3 sync --delete --size-only . s3://parkcenterauto/ --exclude ".git/*" --exclude "*.DS_STORE*" --exclude ".idea/*"
+aws s3 sync --delete --size-only . s3://parkcenterautomotive.com/ --exclude ".git/*" --exclude "*.DS_STORE*" --exclude ".idea/*"
