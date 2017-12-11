48f9d7f247ed379cd5c7b6e61b6eb43a79e0ded1
Davis Ford <ford_davis@bah.com>
2017-12-11 09:56:06 -0500

    update hours

--- js/hours.js
+++ js/hours.js
@@ -5,13 +5,12 @@ $(document).ready(function () {
   const hr_num = d.getHours();
   const s = $('.opening-hours li').eq(day_num)
 
-  // Add active class to the day of the week in the list
-  s.eq(day_num).addClass('active');
+  s.addClass('active'); // Add active class to the day of the week in the list
 
   const day_check = day_num > 0 && day_num < 6  // not mon or sun
   const hr_check = hr_num > 8 && hr_num < 17  // 9 - 5
-  if (day_check && hr_check) {
-    s.eq(day_num).append('<span class="ml-5 badge badge-success">Open Now</span>')
+  if (day_check && hr_check) {  // Prepend the Open badge to the hour span
+    s.children().before('<span class="ml-3 badge badge-success">Open</span>')
   }
 });
 
