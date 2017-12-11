df058fc48bdad01fea08498c4b6a9e126f595980
Davis Ford <ford_davis@bah.com>
2017-12-11 10:37:53 -0500

    more tweaks

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compress images - uncomment to run
-find ./images/ -type f -iname '*.jpg'  -exec jpegoptim --strip-com --max=85 {} \;
-find ./images/ -type f -iname '*.JPG'  -exec jpegoptim --strip-com --max=85 {} \;
-find ./images/ -type f -iname '*.jpeg'  -exec jpegoptim --strip-com --max=85 {} \;
-find ./images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
+#find ./images/ -type f -iname '*.jpg'  -exec jpegoptim --strip-com --max=85 {} \;
+#find ./images/ -type f -iname '*.JPG'  -exec jpegoptim --strip-com --max=85 {} \;
+#find ./images/ -type f -iname '*.jpeg'  -exec jpegoptim --strip-com --max=85 {} \;
+#find ./images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
 
 # Upload to S3
 aws s3 sync --delete --size-only . s3://parkcenterautomotive.com/ --exclude ".git/*" --exclude "*.DS_STORE*" --exclude ".idea/*"
